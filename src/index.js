fd302562e5c3a86e0d5b7f69977440f483bc534f
pawelek <k.pawelek@vp.pl>
2024-02-22 13:00:03 +0100

    add gallery

---
+++ src/index.js
@@ -0,0 +1,111 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const input = document.querySelector('.search-input');
+
+loader.style.display = 'none';
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  gallery.innerHTML = '';
+  loader.style.display = 'block';
+
+  const inputValue = input.value;
+
+  getImages(inputValue)
+    .then(data => {
+      loader.style.display = 'none';
+
+      if (!data.hits.length) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+
+      gallery.innerHTML = createMarkup(data.hits);
+
+      const refreshPage = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      refreshPage.refresh();
+
+      form.reset();
+    })
+    .catch(err => {
+      loader.style.display = 'none';
+      console.log(err);
+    });
+});
+
+function getImages(name) {
+  const key = '42475479-1764a7314469942521760576b';
+
+  if (name.includes(' ')) {
+    name.splice(' ').join('+');
+  }
+
+  const searchParams = new URLSearchParams({
+    key: key,
+    q: name,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return fetch(`https://pixabay.com/api/?${searchParams}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    return res.json();
+  });
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+              width="298"
+            />
+          </a>
+          <div class="container-stats">
+            <div class="block">
+              <h2 class="tittle">Likes</h2>
+              <p class="amount">${likes}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Views</h2>
+              <p class="amount">${views}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Comments</h2>
+              <p class="amount">${comments}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Downloads</h2>
+              <p class="amount">${downloads}</p>
+            </div>
+          </div>
+        </li>`
+    )
+    .join('');
+}
